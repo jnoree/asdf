309f0db9e3225d13ac64c78ab2516b16c7713d7f
John Noree <jnoree@Thomass-Air.hsd1.mn.comcast.net>
2024-11-01 15:00:05 -0500

    rectangle turtle

--- test.py
+++ test.py
@@ -2,18 +2,30 @@ import turtle
 
 def drawRectangle(t, w, h):
     """Get turtle t to draw a rectangle of width w and height h."""
-    for i in range(4):
+    for i in range(2):
         t.forward(w)
         t.left(90)
+        t.forward(h)
+        t.left(90)
 
-def drawSquare(tx, sz):        # a new version of drawSquare
-    drawRectangle(tx, sz, sz)
+def drawRectangleWithSize(tx, width, height):
+    """Draw a rectangle using the turtle with specified width and height."""
+    drawRectangle(tx, width, height)
 
-wn = turtle.Screen()             # Set up the window
+wn = turtle.Screen()  # Set up the window
 wn.bgcolor("lightgreen")
 
-tess = turtle.Turtle()           # create tess
+# Get the underlying Tkinter window
+turtle_window = wn.getcanvas().winfo_toplevel()
 
-drawSquare(tess, 50)
+# Set the window position to the left side of the screen
+x_position = 0  # Change this for different positions
+y_position = 100  # Set desired y position
+turtle_window.geometry("+{}+{}".format(x_position, y_position))
+
+tess = turtle.Turtle()  # create tess
+
+# Draw a rectangle with specified width and height
+drawRectangleWithSize(tess, 100, 50)  # Change these values as needed
 
 wn.exitonclick()
