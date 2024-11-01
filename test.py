0195c0fab971cd9e8893bf4ccfccf28f184c29a0
John Noree <jnoree@Thomass-Air.hsd1.mn.comcast.net>
2024-11-01 14:16:41 -0500

    square turtle

--- test.py
+++ test.py
@@ -1,7 +1,19 @@
-def square(x):
-    y = x * x
-    return y
+import turtle
 
-toSquare = 10
-result = square(toSquare)
-print("The result of", toSquare, "squared is", result)
+def drawRectangle(t, w, h):
+    """Get turtle t to draw a rectangle of width w and height h."""
+    for i in range(4):
+        t.forward(w)
+        t.left(90)
+
+def drawSquare(tx, sz):        # a new version of drawSquare
+    drawRectangle(tx, sz, sz)
+
+wn = turtle.Screen()             # Set up the window
+wn.bgcolor("lightgreen")
+
+tess = turtle.Turtle()           # create tess
+
+drawSquare(tess, 50)
+
+wn.exitonclick()
